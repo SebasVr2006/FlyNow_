669ff6fe0882e3bcf5ed02c7ccc70610efa27b07
SebasVr2006 <sebasvr2006@gmail.com>
2026-06-14 15:04:45 -0300

    Correcciones de JS

--- pages/buscar/buscar.js
+++ pages/buscar/buscar.js
@@ -1,38 +1,23 @@
-document.getElementById("btn-buscar").addEventListener("click", () => {
-    const origen = document.getElementById("origen").value.trim();
-    const destino = document.getElementById("destino").value.trim();
-    const ida = document.getElementById("ida").value;
-    const vuelta = document.getElementById("vuelta").value;
-    const pasajeros = document.getElementById("pasajeros").value;
-    const clase = document.getElementById("clase").value;
-
-    if (!origen || !destino || !ida || !vuelta || !pasajeros) {
-        alert("Por favor completá todos los campos.");
-        return;
+document.querySelector('#form-busqueda').addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (validarFormulario()) {
+        console.log('Redirigiendo...')
     }
-
-    if (origen.toLowerCase() === destino.toLowerCase()) {
-        alert("El origen y el destino no pueden ser iguales.");
-        return;
-    }
-
-    const fechaIda = new Date(ida);
-    const fechaVuelta = new Date(vuelta);
-    const hoy = new Date();
-    hoy.setHours(0, 0, 0, 0);
-
-    if (fechaIda < hoy) {
-        alert("La fecha de ida no puede ser anterior a hoy.");
-        return;
-    }
-
-    if (fechaVuelta < fechaIda) {
-        alert("La fecha de vuelta no puede ser anterior a la de ida.");
-        return;
-    }
-
-    const busqueda = { origen, destino, ida, vuelta, pasajeros, clase };
-    localStorage.setItem("busqueda", JSON.stringify(busqueda));
-
-    window.location.href = "pages/Vuelos/buscar.html";
 });
+
+function mostrarError(id) {
+    document.querySelector(id).classList.add('visible');
+}
+
+function ocultarError(id) {
+    document.querySelector(id).classList.remove('visible');
+}
+
+function validarFormulario() {
+    const origen = document.querySelector('#origen').value;
+    const destino = document.querySelector('#destino').value;
+    const fechaIda = document.querySelector('#ida').value;
+    const fechaVuelta = document.querySelector('#vuelta').value;
+    const pasajeros = document.querySelector('#pasajeros').value;
+    const clase = document.querySelector('#clase').value;
+}
