ed94fa7c086ceeb3224393ec274d82edeb404f8e
SebasVr2006 <sebasvr2006@gmail.com>
2026-06-16 02:55:07 -0300

    Arreglos de JS

--- pages/Vuelos/vuelos.js
+++ pages/Vuelos/vuelos.js
@@ -8,13 +8,15 @@ function generarCards() {
         const div = document.createElement("div");
         div.classList.add("imagen");
 
+        const link = vuelo.destino === 'Argentina' ? './vuelosProvincias.html' : '#';
+
         div.innerHTML = `
-            <a href="#">
+            <a href="${link}">
                 <img class="img-pais" src="../../assets/images/Paises/${vuelo.destino}.jpg" alt="${vuelo.destino}">
                 <div class="info-card">
                     <h4>${vuelo.destino}</h4>
                     <p class="vuelos-desde">Vuelos desde</p>
-                    <p><span>$${vuelo.precio.toLocaleString("es-AR")}</span></p>
+                    <p><span>USD ${vuelo.precio}</span></p>
                 </div>
             </a>
         `;
